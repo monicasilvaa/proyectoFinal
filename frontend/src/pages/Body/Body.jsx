44d08c6e4edf0d893d583afd68fe334a031a9322
monicasilvaa <monicasilva2307@gmail.com>
2024-03-27 03:28:30 +0100

    Front routes and components updated

--- frontend/src/pages/Body/Body.jsx
+++ frontend/src/pages/Body/Body.jsx
@@ -1,5 +1,9 @@
+import { useEffect, useState } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
+import { AdminHeader } from "../../components/AdminHeader/AdminHeader"
+import { Header } from "../../components/Header/Header"
 import PrivateRoute from "../../components/PrivateRoute/PrivateRoute"
+import { CreatePlan } from "../Dietplans/CreatePlan"
 import { Home } from "../Home/Home"
 import Login from "../Login/Login"
 import { Profile } from "../Profile/Profile"
@@ -8,26 +12,59 @@ import { Appointments } from "../User/Appointments"
 import { Dashboard } from "../User/Dashboard"
 import { DietitiansAppointments } from "../User/DietitiansAppointments"
 import { ListAllAppointments } from "../User/ListAllAppointments"
+import { ListAllClients } from "../User/ListAllClients"
 import { ListAllUsers } from "../User/ListAllUsers"
+import { ListDietPlans } from './../Dietplans/ListDietPlans'
 
 export const Body = () => {
 
+    const [isAdminRoute, setIsAdminRoute] = useState(false);
+
+    useEffect(() => {
+        setIsAdminRoute(window.location.pathname.startsWith('/admin'));
+    })
+
     return (
         <>
+            {/*Se carga la cabecera dependiendo de si navegas por el Admin o por el sitio web */}
+            {isAdminRoute ? (
+                <>
+                    <AdminHeader /> 
+                </>
+            )
+            : <Header />}
+
             <Routes>
-                {/* Public routes */}
-                <Route path="*" element={<Navigate to="/" />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login /> } />                
-                <Route path="/register" element={<Register /> } />                
+                <Route path="*" element={<Navigate to="/" />} />                                
                 
-                {/* Private routes */}
-                <Route path="/dashboard" element={<PrivateRoute Component={Dashboard} />} />
-                <Route path="/users" element={<PrivateRoute Component={ListAllUsers} />} />
-                <Route path="/profile" element={<PrivateRoute Component={Profile} />} />
-                <Route path="/myAppointments" element={<PrivateRoute Component={Appointments} />} />
-                <Route path="/dietitianAppointments" element={<PrivateRoute Component={DietitiansAppointments} />} />
-                <Route path="/appointments" element={<PrivateRoute Component={ListAllAppointments} />} />
+                {/* Admin routes */}
+                <Route path="/admin">
+                    <Route path="*" element={<Navigate to="dashboard" />} />
+
+                    {/* Private routes */}
+                    <Route path="dashboard" element={<PrivateRoute Component={Dashboard} />} />
+                    <Route path="users" element={<PrivateRoute Component={ListAllUsers} />} />
+                    <Route path="clients" element={<PrivateRoute Component={ListAllClients} />} />
+                    <Route path="dietplans/create/:clientId" element={<PrivateRoute Component={CreatePlan} />} />
+                    <Route path="dietplans/update/:clientId/:planId" element={<PrivateRoute Component={CreatePlan} />} />
+                    <Route path="dietplans/:clientId" element={<PrivateRoute Component={ListDietPlans} />} />
+                    <Route path="appointments" element={<PrivateRoute Component={ListAllAppointments} />} />
+                </Route>
+
+                {/* Website routes */}
+                <Route path="/">
+                    <Route path="*" element={<Navigate to="/" />} />
+                    {/* Public routes */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="login" element={<Login /> } />                
+                    <Route path="register" element={<Register /> } />
+
+                    {/* Private routes */}
+                    <Route path="profile" element={<PrivateRoute Component={Profile} />} />
+                    <Route path="dietplans/:clientId" element={<PrivateRoute Component={ListDietPlans} />} />
+                    <Route path="myAppointments" element={<PrivateRoute Component={Appointments} />} />
+                    <Route path="dietitianAppointments" element={<PrivateRoute Component={DietitiansAppointments} />} />
+                </Route>
             </Routes>
         </>
     )
