40ee2baa5e30a74b7b531876473fd9a7ecb963cd
monicasilvaa <monicasilva2307@gmail.com>
2024-03-27 03:23:23 +0100

    customInput modification

--- frontend/src/components/CustomInput/CustomInput.jsx
+++ frontend/src/components/CustomInput/CustomInput.jsx
@@ -1,8 +1,8 @@
 import "./CustomInput.css"
 
-export const CustomInput = ({placeholder, type, name, handler, value, required}) => {
+export const CustomInput = ({classname, placeholder, type, name, handler, value, required}) => {
 
     return (
-        <input placeholder={placeholder} type={type} name={name} value={value} required={required} onChange={(e) => handler(e)}></input>
+        <input className={classname} placeholder={placeholder} type={type} name={name} value={value} required={required} onChange={(e) => handler(e)}></input>
     )
 }
