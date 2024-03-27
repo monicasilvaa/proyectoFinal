ed69c6239b39b69f1069e8c172e9ceeaeccf66ca
monicasilvaa <monicasilva2307@gmail.com>
2024-03-27 03:24:55 +0100

    fix headers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
 import { Body } from "./pages/Body/Body";
 
 function App() {
   return (
     <>
-    <Header/>
       <Body />
       <Footer />
     </>
